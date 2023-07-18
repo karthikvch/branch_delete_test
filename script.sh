d7ce0fbb68a180abef708b052789d4fc46b913f6
karthik v ch <109299135+karthikvch@users.noreply.github.com>
2023-07-19 01:04:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
 #!/bin/bash  
 # Bash script  
 echo "Hello World!"  
+
+duration=(gh run list --workflow  python-package.yml -R https://github.com/karthikvch/branch_delete_test --branch kc-2023.07.19 | head -1  |awk 'NR=1 {print $(NF-1)}')
+echo "Duration :$duration"
